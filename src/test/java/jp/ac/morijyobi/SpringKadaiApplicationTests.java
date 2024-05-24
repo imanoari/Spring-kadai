72e95fb45ce3cb6200049ca3c036b6ba51add0a9
imanoari <101164506+imanoari@users.noreply.github.com>
2024-05-24 14:02:42 +0900

    init

---
+++ src/test/java/jp/ac/morijyobi/SpringKadaiApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.ac.morijyobi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKadaiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
